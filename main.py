4d772bd09b79b40c6bb0b2f1f67a4cf5c362fecb
Madeleine Bruce <maxskyebruce@gmail.com>
2023-02-06 13:55:59 -0800

    2-2

--- main.py
+++ main.py
@@ -3,7 +3,18 @@ def main():
     # Comlete your code here
     ##################################################
     pass
-
+reg_hours = 40
+reg_rate = 18.25
+o_rate = 27.78
+total_hours = 50
+overtime_hours = total_hours - reg_hours
+print(overtime_hours)
+regular_wages = reg_hours * reg_rate
+print(regular_wages)
+overtime_wages = overtime_hours * o_rate
+print(overtime_wages)
+total_wages = regular_wages + overtime_wages
+print(total_wages)
 
 if __name__ == '__main__':
     main()
