6449e0263b3d8731786110bd859c447d5c7c9241
Madeleine Bruce <maxskyebruce@gmail.com>
2023-02-06 14:53:41 -0800

    2-2

--- main.py
+++ main.py
@@ -3,14 +3,14 @@ def main():
     # Comlete your code here
     ##################################################
     pass
-workhours = float(50)
+workhours = int(input('Enter your number'))
 reg_hours = 40
 reg_rate = 18.25
-o_rate = 27.78
-overtime_hours = workhours - reg_hours
-regular_charge = reg_hours * reg_rate
-overtime_charge = overtime_hours * o_rate
-print(regular_charge)
+o_rate = 27.7850
+overtime_hours = int(workhours - reg_hours)
+regular_charge = int(reg_hours * reg_rate)
+overtime_charge = int(overtime_hours * o_rate)
+print(regular_charge) 
 print(overtime_charge)
 total_wage = regular_charge + overtime_charge
 print(total_wage)
