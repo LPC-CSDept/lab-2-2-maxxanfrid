bfc803fe33783224a9fb23a8e942c39b97f663ed
Madeleine Bruce <maxskyebruce@gmail.com>
2023-02-06 15:04:12 -0800

    2-2

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ def main():
 workhours = int(input('Enter your number'))
 reg_hours = int(40)
 reg_rate = float(18.25)
-o_rate = float(27.7850)
+o_rate = float(27.78)
 overtime_hours = workhours - reg_hours
 regular_charge = reg_hours * reg_rate
 overtime_charge = overtime_hours * o_rate
