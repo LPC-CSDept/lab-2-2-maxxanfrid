575c2e9a712c94ee555f0e93bf9243c8627a25fa
Madeleine Bruce <maxskyebruce@gmail.com>
2023-02-06 15:33:16 -0800

    2-2

--- main.py
+++ main.py
@@ -2,18 +2,17 @@ def main():
     ##################################################
     # Comlete your code here
     ##################################################
-    pass
-workhours = int(input('Enter your number'))
-reg_hours = int(40)
-reg_rate = float(18.25)
-o_rate = float(27.78)
-overtime_hours = workhours - reg_hours
-regular_charge = reg_hours * reg_rate
-overtime_charge = overtime_hours * o_rate
-print(regular_charge) 
-print(overtime_charge)
-total_wage = regular_charge + overtime_charge
-print(total_wage)
-
+    workhours = int(input('Enter your number'))
+    reg_hours = 40
+    reg_rate = float(18.25)
+    o_rate = float(27.78)
+    overtime_hours = workhours - reg_hours
+    regular_charge = reg_hours * reg_rate
+    overtime_charge = overtime_hours * o_rate
+    print(regular_charge) 
+    print(overtime_charge)
+    total_wage = regular_charge + overtime_charge
+    print(total_wage)
+pass 
 if __name__ == '__main__':
     main()
